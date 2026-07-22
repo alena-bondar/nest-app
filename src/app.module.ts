dd7a5dfa5e4b6e9bc6814920f6ee9f39a7991d93
alena <bonda5229@gmail.com>
2026-07-22 11:36:05 +0200

    add user and document modules

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { DocumentsModule } from './documents/documents.module';
+
+@Module({
+  imports: [UsersModule, DocumentsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
